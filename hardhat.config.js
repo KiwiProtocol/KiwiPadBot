32cd712fd9080e4dcd4631fd9e5562cbdd5445a4
Tomiwa Adeyemi <adeyemitomiwa7@gmail.com>
2024-08-06 00:54:35 +0100

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-verify");
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.20", //replace your own solidity compiler version
+  networks: {
+    opbnb: {
+      url: "https://opbnb-mainnet-rpc.bnbchain.org/",
+      chainId: 5611, // Replace with the correct chainId for the "opbnb" network
+      accounts: ["{{YOUR-PRIVATE-KEY}}"], // Add private keys or mnemonics of accounts to use
+      gasPrice: 20000000000,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      opbnb: "{{YOUR-NODEREAL-API-KEY}}", //replace your nodereal API key
+    },
+
+    customChains: [
+      {
+        network: "opbnb",
+        chainId: 5611, // Replace with the correct chainId for the "opbnb" network
+        urls: {
+          apiURL:
+            "https://open-platform.nodereal.io/{{YOUR-NODEREAL-API-KEY}}/op-bnb-mainnet/contract/",
+          browserURL: "https://opbnbscan.com/",
+        },
+      },
+    ],
+  },
 };
